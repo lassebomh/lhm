4e17720a432a8ad05d4149b019004d7ad39b064b
Lasse H. Bomholt <lasse@bomh.net>
2024-10-02 19:52:32 +0200

    First hm commit

---
+++ home.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... }:
+{
+  home = {
+    packages = with pkgs; [
+      hello
+    ];
+
+    username = "lasse";
+    homeDirectory = "/home/lasse";
+
+    stateVersion = "23.11";
+  }
+}
