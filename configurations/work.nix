90b1ab8cbcd8cf0ceb6e35cc5d9f164dac2199d8
Lasse H. Bomholt <lasse@bomh.net>
2024-10-24 18:34:24 +0200

    asdf

--- configurations/work.nix
+++ configurations/work.nix
@@ -41,9 +41,10 @@
 
     direnv = {
       enable = true;
+      enableBashIntegration = true;
       enableZshIntegration = true;
-      nix-direnv.enable = true;
-      silent = true;
+      nix-direnv.enable = false;
+      # silent = true;
     };
         
     fzf = {
@@ -60,36 +61,6 @@
     neovim = {
       enable = true;
       defaultEditor = true;
-    };
-
-    tmux = {
-      enable = true;
-      mouse = true;
-      extraConfig = ''
-        # Change the prefix key to C-a
-        unbind C-b
-        set -g prefix C-a
-        bind C-a send-prefix
-
-        # Use Alt-arrow keys without prefix key to switch panes
-        bind -n M-Left select-pane -L
-        bind -n M-Right select-pane -R
-        bind -n M-Up select-pane -U
-        bind -n M-Down select-pane -D
-
-        # Shift arrow to switch windows
-        bind -n S-Left  previous-window
-        bind -n S-Right next-window
-
-        # Use | and - to split windows
-        bind | split-window -h
-        bind - split-window -v
-        unbind '"'
-        unbind %
-
-        # Kill without confirmation
-        bind-key x kill-pane
-      '';
     };
    };
 }
